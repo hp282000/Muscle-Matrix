f4711c0c21b63b530d2eff298a606e64813b0e08
Harsh <harsp24@gmail.com>
2023-03-10 14:41:06 +0530

    Unit Testing for user table

--- MuscleMatrix.Infrastructure.Contract/IUserRepository.cs
+++ MuscleMatrix.Infrastructure.Contract/IUserRepository.cs
@@ -5,7 +5,7 @@ namespace MuscleMatrix.Infrastructure.Contract
 {
     public interface IUserRepository
     {
-        Task<User> UserLogin(UserLogin userLogin);
+ Task<User> UserLogin(UserLogin userLogin);       
         Task<int> AddUser(User user);
 
         Task<List<User>> GetAllUsers();
@@ -14,6 +14,6 @@ namespace MuscleMatrix.Infrastructure.Contract
 
         Task<User> UpdateUser(User user);
 
-        Task<User> GeUserById(int id);
+        Task<User> GetUserById(int id);
     }
 }
